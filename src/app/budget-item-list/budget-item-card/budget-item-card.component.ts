87f55a8f3129d97c5433b93f185fc44ddf755739
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2020-01-01 15:37:58 +0200

    adding material dialog

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component,Inject, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { MatDialog,MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EditItemModalComponent } from 'src/app/edit-item-modal/edit-item-modal.component';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -10,7 +12,7 @@ export class BudgetItemCardComponent implements OnInit {
 
   @Input() item:BudgetItem;
   @Output() deletedItem: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>()
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
@@ -18,4 +20,17 @@ export class BudgetItemCardComponent implements OnInit {
   deleteItem(){
     this.deletedItem.emit();
   }
+  openDialog(item:BudgetItem){
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '600px',
+      data: item
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
+  }
+
 }
+
