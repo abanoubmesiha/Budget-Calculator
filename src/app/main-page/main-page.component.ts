5e1d38ea6c31f0c59d91b76d93dd8d09fbf80c48
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2020-01-01 19:40:27 +0200

    update total when edit

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { isNgTemplate } from '@angular/compiler';
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 
 @Component({
   selector: 'app-main-page',
@@ -8,15 +10,23 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 })
 export class MainPageComponent implements OnInit {
   budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  Total:number = 0;
   constructor() { }
 
   ngOnInit() {
   }
   addItems(newItem:BudgetItem){
     this.budgetItems.push(newItem);
+    this.Total += newItem.amount;
   }
   deleteItem(item:BudgetItem){
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index,1);
+    this.Total -= item.amount;
+  }
+  updateItem(updateEvent:UpdateEvent){
+    this.budgetItems[this.budgetItems.indexOf(updateEvent.old)]=updateEvent.new;
+    this.Total -= updateEvent.old.amount;
+    this.Total += updateEvent.new.amount;
   }
 }
